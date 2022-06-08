f562d5d53a3e29bc794cfd871362aa06dcf66f5c
ashwyn28 <ashwyn28annauth@gmail.com>
2022-06-09 00:13:13 +0100

    pay: fix redirect and remove checkout

--- work/urls.py
+++ work/urls.py
@@ -4,6 +4,5 @@ from work import views
 urlpatterns = [
     path("create/", views.create_listing),
     path("listings/", views.Listings.as_view()),
-    path("pay/", views.create_checkout_session),
     path("elements/", views.create_payment_intent),
 ]
--- work/views.py
+++ work/views.py
@@ -52,35 +52,14 @@ class Filters(ListAPIView):
 
 class Companies(ListAPIView):
     # TODO: annotate/group all listings by company_name
-
     pass
 
-@api_view(["POST"])
-def create_checkout_session(request):
-    session = stripe.checkout.Session.create(
-        line_items=[
-            {
-                "price_data": {
-                    "currency": "usd",
-                    "product_data": {
-                        "name": "Listing",
-                    },
-                    "unit_amount": 19900,
-                },
-                "quantity": 1,
-            }
-        ],
-        mode="payment",
-        success_url="http://localhost:3000/pay/success",
-        cancel_url="https://example.com/cancel",
-    )
-
-    return redirect(session.url, code=303)
-
-
 def calculate_order_amount(items):
-    breakpoint()
-    return 1400
+    for item in items:
+        if item['id'] == 'basic':
+            return 1400
+        if item['id'] == 'premium':
+            return 2000
 
 @api_view(["POST"])
 def create_payment_intent(request):
